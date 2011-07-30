3dce671b47f19d3935f31ed415aebba337ffa158
Peter Odding <peter@peterodding.com>
2011-07-30 22:54:43 +0200

    Update .gitignore, add example

---
+++ examples/folding.py
@@ -0,0 +1,34 @@
+# Block
+# comment
+# on
+# multiple
+# lines.
+
+def test2():
+    pass
+    pass
+    pass
+
+def test(test):
+    """
+    This is the
+    docstring!
+    """
+    pass
+
+class Test:
+
+    def foo():
+        ''' docstring for foo() '''
+        pass
+
+    def bar():
+        '''
+        docstring for bar()
+        '''
+        pass
+    def baz():
+        '''
+        docstring for baz()
+        '''
+        pass
